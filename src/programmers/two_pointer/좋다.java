59cff6c6b54d3d2ed6a487b35faf135ae9904cc3
ChanUi <hcu55@naver.com>
2024-07-25 09:09:54 +0900

    :sparkles: 좋다(two_pointer), 골드 IV

---
+++ src/programmers/two_pointer/좋다.java
@@ -0,0 +1,46 @@
+package programmers.two_pointer;
+
+import java.util.*;
+import java.io.*;
+
+// 좋다, 골드 IV
+public class 좋다 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);   // 배열 정렬
+
+        int result = 0;
+        for (int k = 0; k < N; k++) {
+            int find = arr[k];  // 찾으려는 수
+            int i = 0;          // 포인터
+            int j = N - 1;
+
+            while (i < j) {
+                if (arr[i] + arr[j] == find) {
+                    if (i != k && j != k) {
+                        result++;
+                        break;
+                    } else if (i == k) {
+                        i++;
+                    } else if (j == k) {
+                        j--;
+                    }
+                } else if (arr[i] + arr[j] < find) {
+                    i++;
+                } else {
+                    j--;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
