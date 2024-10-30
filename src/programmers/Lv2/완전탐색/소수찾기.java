4c6a5a0da640228eb29383441c60e0dc73afde9b
ChanUi <hcu55@naver.com>
2024-10-30 22:51:20 +0900

    소수 찾기, Programmers(Level 2)

---
+++ src/programmers/Lv2/완전탐색/소수찾기.java
@@ -0,0 +1,53 @@
+package programmers.Lv2.완전탐색;
+
+import java.util.*;
+
+// 소수 찾기, Programmers(Level 2)
+public class 소수찾기 {
+    static ArrayList<Integer> arr = new ArrayList<>();
+    static boolean[] check = new boolean[7];
+
+    public int solution(String numbers) {
+        int answer = 0;
+
+        for (int i = 0; i < numbers.length(); i++) {
+            dfs(numbers, "", i + 1);
+        }
+
+        for (int i = 0; i < arr.size(); i++) {
+            if (prime(arr.get(i))) answer++;
+        }
+
+        return answer;
+    }
+
+    static void dfs(String str, String temp, int m) {
+        if (temp.length() == m) {
+            int num = Integer.parseInt(temp);
+            if (!arr.contains(num)) {
+                arr.add(num);
+            }
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (!check[i]) {
+                check[i] = true;
+                temp += str.charAt(i);
+                dfs(str, temp, m);
+                check[i] = false;
+                temp = temp.substring(0, temp.length() - 1);
+            }
+        }
+    }
+
+    static boolean prime(int n) {
+        if (n < 2) return false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
