ce72e09082e16b667910df4462d299ce954a13ef
ChanUi <hcu55@naver.com>
2025-01-29 23:55:18 +0900

    숨박꼭질(graph), 실버 I

---
+++ src/baekjoon/graph/숨박꼭질.java
@@ -0,0 +1,56 @@
+package baekjoon.graph;
+
+import java.io.*;
+import java.util.*;
+
+// 숨박꼭질(graph), 실버 I
+public class 숨박꼭질 {
+    static int N;
+    static int K;
+    static int[] visited = new int[100001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        if (N == K) {
+            System.out.println(0);
+        } else {
+            bfs(N, K);
+        }
+    }
+
+    static void bfs(int N, int K) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(N);
+        visited[N] = 1;
+
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+
+            for (int i = 0; i < 3; i++) {
+                int next;
+
+                if (i == 0) {
+                    next = now + 1;
+                } else if (i == 1) {
+                    next = now - 1;
+                } else {
+                    next = now * 2;
+                }
+
+                if (next == K) {
+                    System.out.println(visited[now]);
+                    return;
+                }
+
+                if (next >= 0 && next < visited.length && visited[next] == 0) {
+                    queue.add(next);
+                    visited[next] = visited[now] + 1;
+                }
+            }
+        }
+    }
+}
