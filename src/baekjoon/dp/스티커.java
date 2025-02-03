605a132709b5a04eb900b2c465c9acec19a3cfd8
ChanUi <hcu55@naver.com>
2025-02-03 17:41:19 +0900

    스티커(dp), 실버 I

---
+++ src/baekjoon/dp/스티커.java
@@ -0,0 +1,36 @@
+package baekjoon.dp;
+
+import java.io.*;
+import java.util.*;
+
+// 스티커(dp), 실버 I
+public class 스티커 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int testCase = 0; testCase < T; testCase++) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] dp = new int[2][n + 1];
+            int[][] sticker = new int[2][n + 1];
+
+            for (int i = 0; i < 2; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 1; j <= n; j++) {
+                    sticker[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            dp[0][1] = sticker[0][1];
+            dp[1][1] = sticker[1][1];
+
+            for (int i = 2; i <= n; i++) {
+                dp[0][i] = Math.max(dp[1][i - 2], dp[1][i - 1]) + sticker[0][i];
+                dp[1][i] = Math.max(dp[0][i - 2], dp[0][i - 1]) + sticker[1][i];
+            }
+            System.out.println(Math.max(dp[0][n], dp[1][n]));
+        }
+    }
+}
